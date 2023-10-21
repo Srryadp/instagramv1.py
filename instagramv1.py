6e36415d5884a23acb0165e4410698cc115d31df
Srryadp <143006029+Srryadp@users.noreply.github.com>
2023-10-21 16:16:04 -0500

    instagramv1.py

--- instagramv1.py
+++ instagramv1.py
@@ -12,6 +12,7 @@ locations = [
 
 
 class FeedPost:
+
     """This is a docstring to document code.
     This class is about the posts that goes inside
     the newsfeed of Instagram"""
@@ -33,62 +34,114 @@ class FeedPost:
     ]
 
     def get_location(self):
+ 
         # print("Called by {}".format(self))
+ 
         return random.sample(locations, 1)[0]
 
     def get_comments(self):
+ 
         if self.comments:
+ 
             return self.comments
+ 
         else:
+ 
             return "This post has no comments"
 
     def get_likes(self):
+ 
         return self.likes
 
     def show_filters(self):
+
         print('Available filters are:')
+
         for the_filter in self.filters:
+
             print('*', the_filter)
 
     def load_image(self):
+
         print("Write/Paste your image.") 
         print("Use Ctrl-D or Ctrl-Z ( windows ) to save it.")
+
         while True:
+
             try:
+
                 line = input()
+
             except EOFError:
+
                 break
+
             else:
+
                 self.image.append(line)
         try:
+
             if line:
+
                 return True
+
         except UnboundLocalError:
+
             print('blank image')
+
             return False
 
     def show_image(self):
+
         if self.image:
+
             # print('The image is:')
+            
             print('')
+
             for line in self.image:
+
                 if line:
+
                     print(line)
             print('')
 
+    def show_tags(self):
+        
+        if self.tags:
+            
+            # print('The image is:')
+            
+            print('')
+            
+            for line in self.tags:
+                if line:
+                    print(line)
+            
+            print('')
+        
+
     def tag_people(self):
+        
         print('')
         print("Write your friends's names one by one.") 
         print("Use Ctrl-D or Ctrl-Z ( windows ) to save it.")
         print('')
+        
         while True:
+            
             try:
+
                 line = input('> ')
+            
             except EOFError:
+                
                 break
+            
             self.tags.append(line)
 
     def post(self):
+        
         """
         Post method for the FeedPost class
         
@@ -98,9 +151,13 @@ class FeedPost:
         """
 
         if self.load_image():
+            
             print("Image saved successfully")
+        
         else:
+            
             print('No image was uploaded.')
+            
             return False
 
         self.location = self.get_location()
@@ -127,16 +184,22 @@ class FeedPost:
             print(self.description)
             print(self.likes, 'likes')
             self.get_comments()
-            print('tags:', self.tags)
+            print('tags:')
+            self.show_tags()
             print('\n\n')     
+        
         else:
+            
             return False
     
     def edit(self):
+        
         pass
 
     def image_filter(self):
+        
         pass
 
     def delete(self):
+        
         pass
