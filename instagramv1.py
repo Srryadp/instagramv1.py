3742dc11d474fd3743141a28c0d40b924aec405e
Srryadp <143006029+Srryadp@users.noreply.github.com>
2023-10-21 17:33:20 -0500

    instagramv1.py

--- instagramv1.py
+++ instagramv1.py
@@ -40,14 +40,18 @@ class FeedPost:
         return random.sample(locations, 1)[0]
 
     def get_comments(self):
- 
-        if self.comments:
- 
-            return self.comments
- 
-        else:
- 
-            return "This post has no comments"
+            
+            while True:
+            
+                try:
+
+                    line = input('> ')
+                
+                except EOFError:
+                    
+                    break
+                
+                self.comments.append(line)
 
     def get_likes(self):
  
@@ -104,23 +108,42 @@ class FeedPost:
                 if line:
 
                     print(line)
+            
             print('')
 
     def show_tags(self):
         
         if self.tags:
             
-            print('\nTags:')
+            print('\ntags:')
             
             print('')
             
             for nombres in self.tags:
-                    
-                print("* ", nombres)
+                
+                    print("* ", nombres)
+            
+            print('')
+    
+
+    def show_comments(self):
+
+        if self.comments:
+            
+            print("\nComments in this post:")
+
+            for comments in self.comments:
+
+                print("* ", comments)
             
             print('')
         
+        else:
+            
+            print("\nNo commments in this post")
 
+            return False     
+    
     def tag_people(self):
         
         print('')
@@ -186,6 +209,8 @@ class FeedPost:
             self.get_comments()
             print('tags:')
             self.show_tags()
+            print('Com')
+            self.show_comments()
             print('\n\n')     
         
         else:
@@ -194,12 +219,90 @@ class FeedPost:
     
     def edit(self):
         
-        pass
+        if self.image:
+
+            self.image.remove()
+
+            if self.image == False:
+                 
+                 while True:
+
+                    try:
+
+                        line = input()
+
+                    except EOFError:
+
+                        break
+
+                    else:
+
+                        self.image.append(line)
+                    
+                    try:
+
+                        if line:
+
+                            return True
+                    
+                    except UnboundLocalError:
+
+                        print('blank image')
+
+                    return False
 
     def image_filter(self):
         
-        pass
+        print('')
 
-    def delete(self):
+        for the_filter in self.filters:
+
+            print('*', the_filter)
+
+        if self.image:
+            
+            self.image.remove()
+            
+            while True:
+
+                try:
+
+                    line = input()
+
+                except EOFError:
+
+                    break
+
+                else:
+
+                    self.image.append(line)
+            try:
+
+                if line:
+
+                    return True
+            
+            except UnboundLocalError:
+
+                print('blank image')
+
+            return False
+
+    def delete_image(self):
         
-        pass
+        if self.image:
+            
+            usuario = input("Desea borrar la imagen? Y/N: ")
+        
+            if usuario == "Y":
+                
+                 self.image.remove()
+
+            return False 
+
+        else:
+
+            print("\nNo hay imagen que borrar")    
+
+            return False
+            
