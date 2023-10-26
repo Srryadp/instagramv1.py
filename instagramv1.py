d0e61059869aade3a289ff927fc5fe52a4f70446
Srryadp <143006029+Srryadp@users.noreply.github.com>
2023-10-25 22:19:23 -0500

    instagramv2.py

--- instagramv1.py
+++ instagramv1.py
@@ -12,16 +12,14 @@ locations = [
 
 
 class FeedPost:
-
-    """This is a docstring to document code.
+    """
+    --- This is a docstring to document code. ---
     This class is about the posts that goes inside
-    the newsfeed of Instagram"""
-    image = []
+    the newsfeed of Instagram.
+    """
     description = None
     location = None
     likes = 0
-    comments = []
-    tags = []
     filters = [
         'MayFair',
         'Amaro',
@@ -30,45 +28,83 @@ class FeedPost:
         'Moon',
         'Lark',
         'Reyes',
-        'Perpetua'
+        'Perpetua',
     ]
 
-    def get_location(self):
+    def __init__(self, location=None, description=None, tag=None, **kwargs):
  
-        # print("Called by {}".format(self))
+        if location:
  
+            self.location = location
+ 
+        if description:
+ 
+            self.description = description
+ 
+        if tag:
+ 
+            self.tags.append(tag)
+ 
+        self.image = []
+        self.comments = []
+        self.tags = []
+        self.author = None
+
+        for llave, valor in kwargs.items():
+
+            setattr(self, llave, valor)
+
+    def get_location(self, position=None):
+        '''
+        Position is an int
+        '''
+
+        if position:
+
+            try:
+
+                return locations[position]
+
+            except IndexError:
+
+                print('The position is out of the range')
+
+                return None
+
+            except TypeError:
+
+                print('Please write just integer numbers')
+
+                return None
+
         return random.sample(locations, 1)[0]
 
     def get_comments(self):
-            
-            while True:
-            
-                try:
 
-                    line = input('> ')
-                
-                except EOFError:
-                    
-                    break
-                
-                self.comments.append(line)
+        if self.comments:
+
+            return self.comments
+
+        else:
+
+            return "This post has no comments"
 
     def get_likes(self):
- 
+
         return self.likes
 
     def show_filters(self):
 
-        print('Available filters are:')
+        print('* Available filters are:')
 
-        for the_filter in self.filters:
+        for filter in self.filters:
 
-            print('*', the_filter)
+            print('  -', filter)
 
     def load_image(self):
 
-        print("Write/Paste your image.") 
-        print("Use Ctrl-D or Ctrl-Z ( windows ) to save it.")
+        print('Write/Paste your image.')
+        print('Use Ctrl-D or Ctrl-Z ( windows ) to save it.')
 
         while True:
 
@@ -83,6 +119,7 @@ class FeedPost:
             else:
 
                 self.image.append(line)
+
         try:
 
             if line:
@@ -91,16 +128,19 @@ class FeedPost:
 
         except UnboundLocalError:
 
-            print('blank image')
+            print('Blank image')
 
             return False
+
+    def apply_filter(self):
+
+        pass
 
     def show_image(self):
 
         if self.image:
 
-            # print('The image is:')
-            
+            print('* Image:')
             print('')
 
             for line in self.image:
@@ -108,201 +148,206 @@ class FeedPost:
                 if line:
 
                     print(line)
-            
+
             print('')
 
-    def show_tags(self):
-        
-        if self.tags:
-            
-            print('\ntags:')
-            
-            print('')
-            
-            for nombres in self.tags:
-                
-                    print("* ", nombres)
-            
-            print('')
-    
-
-    def show_comments(self):
-
-        if self.comments:
-            
-            print("\nComments in this post:")
-
-            for comments in self.comments:
-
-                print("* ", comments)
-            
-            print('')
-        
-        else:
-            
-            print("\nNo commments in this post")
-
-            return False     
-    
     def tag_people(self):
-        
+
         print('')
-        print("Write your friends's names one by one.") 
+        print("Write your friends's names one by one.")
         print("Use Ctrl-D or Ctrl-Z ( windows ) to save it.")
         print('')
-        
+
         while True:
-            
+
             try:
 
                 line = input('> ')
-            
+
             except EOFError:
-                
+
                 break
-            
+
             self.tags.append(line)
 
-    def post(self):
-        
-        """
-        Post method for the FeedPost class
-        
-        input: nothing
-        output: Returns True if everything was created successfully
-                or False in case of an exception happened.
-        """
+
+    def create(self, user):
 
         if self.load_image():
-            
-            print("Image saved successfully")
-        
+
+            print('* Image saved successfully')
+
         else:
-            
-            print('No image was uploaded.')
-            
+
+            print('No image was uploaded')
+
             return False
 
-        self.location = self.get_location()
-        self.description = input('Please write a description: ')
-        self.tag_people()
-        
-        '''
         if not self.location:
-            self.location = self.get_location()
+
+            opcion = int(input('Choose:\n1. Write a location\n 2. Get a location randomly\n> '))
+            
+            if opcion == 1:
+
+                chosen_location = input('Please write a location for your post')
+
+            else:
+
+                chosen_location = self.get_location()
+
+            self.location = chosen_location
+
         if not self.description:
-            print('Your current description is ', self.description)
+
             self.description = input('Please write a description: ')
-        '''
+
+        self.tag_people()
+        self.author = user
+        user.posts.append(self)
+
+        return self
+
+    def read(self):
+ 
+        print('\n\n')
+        print('* Location:', self.location)
+        self.show_image()
+        print('* Description:', self.description)
+        print('* Likes:', self.get_likes())
+        print('* Comments:', self.get_comments())
+        print('* Tags:', self.tags)
+        print('\n\n')
+
+    def update(self):
+       
+        pass
+
+    def delete(self):
+       
+        pass
+
+
+class User:
+    
+    ''' Instagram User '''
+    
+    followers = 0
+    
+    following = 0
+
+    def __init__(self, username, bio=None, **kwargs):
+       
+        self.username = username
+       
+        if bio:
+       
+            self.bio = bio
+       
+        else:
+       
+            self.bio = None
+       
+        self.image = []
+        self.posts = []
+
+       
+        for key, value in kwargs.items():
+       
+            setattr(self, key, value)
+
+       
+        if self.load_image():
+       
+            print('* Profile image uploaded successfully \n\n')
+       
+        else:
+       
+            print('No profile image was uploaded \n\n')
+
+    def load_image(self):
+        
+        print('Write/Paste your image.')
+        
+        print('Use Ctrl-D or Ctrl-Z ( windows ) to save it.')
+
+        while True:
+        
+            try:
+        
+                line = input()
+        
+            except EOFError:
+        
+                break
+        
+            else:
+        
+                self.image.append(line)
+
+        try:
+        
+            if line:
+        
+                return True
+        
+        except UnboundLocalError:
+        
+            print('Blank image')
+        
+            return False
+
+    def show_image(self):
+        
+        if self.image:
+           
+            print('* Image:')
+            print('')
+           
+            for line in self.image:
+
+                if line:
+                    print(line)
+
+            print('')
+
+
+    def give_likes(self, post):
+
+        post.likes += 1
 
         return True
 
+
+    def read_posts(self, feed):
+
+        for publication in feed:
+
+            publication.read()
+
+            print('\n\n --------------- \n\n')
+
+    def give_comments(self):
+
+        pass
+
+    def create(self):
+
+        pass
+
     def read(self):
+
+        print('\n\n')
+        print('* Username:', self.username)
+        self.show_image()
+        print('* Biography:', self.bio)
+        print('* Followers:', self.followers)
+        print('* Following:', self.following)
+        print('\n\n')
+
+    def update(self):
         
-        if self.image:
-            
-            print('\n\n')
-            print(self.location)
-            self.show_image()
-            print(self.description)
-            print(self.likes, 'likes')
-            self.get_comments()
-            print('tags:')
-            self.show_tags()
-            print('Com')
-            self.show_comments()
-            print('\n\n')     
+        pass
+
+    def delete(self):
         
-        else:
-            
-            return False
-    
-    def edit(self):
-        
-        if self.image:
-
-            self.image.remove()
-
-            if self.image == False:
-                 
-                 while True:
-
-                    try:
-
-                        line = input()
-
-                    except EOFError:
-
-                        break
-
-                    else:
-
-                        self.image.append(line)
-                    
-                    try:
-
-                        if line:
-
-                            return True
-                    
-                    except UnboundLocalError:
-
-                        print('blank image')
-
-                    return False
-
-    def image_filter(self):
-        
-        print('')
-
-        for the_filter in self.filters:
-
-            print('*', the_filter)
-
-        if self.image:
-            
-            self.image.remove()
-            
-            while True:
-
-                try:
-
-                    line = input()
-
-                except EOFError:
-
-                    break
-
-                else:
-
-                    self.image.append(line)
-            try:
-
-                if line:
-
-                    return True
-            
-            except UnboundLocalError:
-
-                print('blank image')
-
-            return False
-
-    def delete_image(self):
-        
-        if self.image:
-            
-            usuario = input("Desea borrar la imagen? Y/N: ")
-        
-            if usuario == "Y":
-                
-                 self.image.remove()
-
-            return False 
-
-        else:
-
-            print("\nNo hay imagen que borrar")    
-
-            return False
-            
+        pass
